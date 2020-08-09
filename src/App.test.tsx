936abf86e5a3366e5d2f7070577c22c8cb363d75
Hamzah-syed <50793209+Hamzah-syed@users.noreply.github.com>
2020-08-10 01:45:25 +0500

    first test pass

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import App from "./App";
+import { shallow } from "enzyme";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("counter testing", () => {
+  test("testing app div render", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("div").length).toEqual(1);
+  });
 });
